07bddb064602e17a807f83c7e2a00470d24f3263
antonioarf <antonioarf@al.insper.edu.br>
2021-06-02 15:09:57 -0300

    teste heroku

--- staticfiles/notes/csss/getit.js
+++ staticfiles/notes/csss/getit.js
@@ -1,3 +1,8 @@
+// const axios = require("axios");
+// const async = require('async');
+// const await = require('await');
+
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -19,12 +24,30 @@ function func2(nome1,nome2,nome3){
   console.log("entrouuuuuuuuuu2")
 }
 
-function func3(){
-  // document.getElementById(nome1).style.display = "none";
-  // document.getElementById(nome2).style.display = "block";
-  // document.getElementById(nome3).style.transform = "none";
-  // document.getElementById("update").disable = false;
-  console.log("entrouuuuuuuuuu2")
+function func3(inp){
+  document.getElementById("myPopup").style.display = 'block';
+  document.getElementById("idid").value = inp;
+  console.log(inp);
+}
+
+async function func4(){
+  console.log("12345678901234567890")
+  lista = await axios.get('http://127.0.0.1:8000/api/users/' );
+  console.log(lista.data)
+  var x = document.forms["myForms"]["usuario"].value;
+  var id = document.forms["myForms"]["custId"].value;
+  if (lista.data.includes(x)) {
+    const options1 = {headers: {'Content-Type': 'application/json', 'Accept': 'application/json'}};
+    // lista = await axios.post('http://127.0.0.1:8000/api/share/' );
+    tes =  axios.post('http://127.0.0.1:8000/api/share/' ,{nome :x, id :id  } , options1);
+    console.log(tes)
+    alert("Nota compartilhada com sucesos")
+    document.getElementById("myPopup").style.display = 'none';
+  }
+  else {
+    alert("Usuario nao encontrado");
+    return false;
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -42,6 +65,7 @@ document.addEventListener("DOMContentLoaded", function () {
     textarea.addEventListener("input", autoResize, false);
   }
 
+
   // Sorteia classes de cores aleatoriamente para os cards
   let cards = document.getElementsByClassName("card");
   for (let i = 0; i < cards.length; i++) {
@@ -51,4 +75,35 @@ document.addEventListener("DOMContentLoaded", function () {
       5
     )} card-rotation-${getRandomInt(1, 6)}`;
   }
+  console.log("testeeeeee")
+
+  let botoes = document.getElementsByClassName("botao_mudar");
+  Array.prototype.forEach.call(botoes, function(item) {
+    console.log("@@@@@@@@@@@@@@@@@@@@")
+      if  (item.innerHTML == " Feito: True" ){
+        item.innerHTML = "Concluido";
+        item.style.color = "blue";
+        console.log("$$$$$$$$$$$$$$$$$$$")
+      }
+      else if  (item.innerHTML == " Feito: False" ){
+        console.log("%%%%%%%%%%%%%%%%%%%%%")
+        item.innerHTML = "Pendente";
+        item.style.color = "red";
+
+      }
+  });
+
+  let dates = document.getElementsByClassName("card-date");
+
+  Array.prototype.forEach.call(dates, function(item) {
+    try {
+    item.innerHTML = item.innerHTML.split("-").reverse().join("/");
+    }
+    catch(err) {
+      console.log("DDDDDDDDDDDD");
+
+    }
+});
+
+
 });
